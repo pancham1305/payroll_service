b0b79b034fdcdfcd82988b38db841bee0a8d2a4c
Pancham Mehta <pancham1305@gmail.com>
2025-01-11 14:52:41 +0530

    Initial Setup

---
+++ payroll-service/src/main/java/com/payrollservice/App.java
@@ -0,0 +1,13 @@
+package com.payrollservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
